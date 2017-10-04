b24208cadec50d33511bdca1967f477e62804060
DJ_PC <dpandeyk.93@gmail.com>
2017-10-05 00:47:03 +0530

    Dheeraj First Commit-05/10/2017

--- app/src/main/java/com/localfriend/MainActivity.java
+++ app/src/main/java/com/localfriend/MainActivity.java
@@ -1,65 +1,155 @@
 package com.localfriend;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.localfriend.adapter.DrawerAdapter;
 import com.localfriend.application.MyApp;
+import com.localfriend.fragments.CenteredTextFragment;
 import com.localfriend.fragments.FragmentDrawer;
+import com.localfriend.fragments.TiffinFragment;
+import com.localfriend.utils.DrawerItem;
+import com.localfriend.utils.SimpleItem;
+import com.localfriend.utils.SpaceItem;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends CustomActivity implements FragmentDrawer.FragmentDrawerListener{
-    private FragmentDrawer drawerFragment;
-    private DrawerLayout drawer;
-    private TextView tv_home, tv_tiffin, tv_cart, tv_more;
-    private ImageButton navBtn;
+public class MainActivity extends CustomActivity implements DrawerAdapter.OnItemSelectedListener {
+    private static final int POS_DASHBOARD = 0;
+    private static final int POS_ACCOUNT = 1;
+    private static final int POS_MESSAGES = 2;
+    private static final int POS_CART = 3;
+    private static final int POS_LOGOUT = 5;
+
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+
+    private SlidingRootNav slidingRootNav;
+
+    //private TextView tv_home, tv_tiffin, tv_cart, tv_more;
+    private ImageView img_home, img_tiffin, img_cart, img_more;
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
+
+    // private ImageButton navBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        slidingRootNav = new SlidingRootNavBuilder(this)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.menu_left_drawer)
+                .inject();
+
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
+
+        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_DASHBOARD).setChecked(true),
+                createItemFor(POS_ACCOUNT),
+                createItemFor(POS_MESSAGES),
+                createItemFor(POS_CART),
+                new SpaceItem(48),
+                createItemFor(POS_LOGOUT)));
+        adapter.setListener(this);
+
+        RecyclerView list = findViewById(R.id.list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+
+        adapter.setSelected(POS_DASHBOARD);
+
+
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.service_container, new TiffinFragment()).commit();
 
         setupUiElements();
 
 
+    }
 
+    @Override
+    public void onItemSelected(int position) {
+        if (position == POS_LOGOUT) {
+            finish();
+        }
+        slidingRootNav.closeMenu();
+        Fragment selectedScreen = CenteredTextFragment.createFor(screenTitles[position]);
+        showFragment(selectedScreen);
+    }
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        DrawerLayout.LayoutParams lll = (DrawerLayout.LayoutParams) drawer.getLayoutParams();
+    private void showFragment(Fragment fragment) {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
 
-//        lll.set
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+    private DrawerItem createItemFor(int position) {
+        return new SimpleItem(screenIcons[position], screenTitles[position])
+                .withIconTint(color(R.color.black))
+                .withTextTint(color(R.color.black))
+                .withSelectedIconTint(color(R.color.colorAccent))
+                .withSelectedTextTint(color(R.color.colorAccent));
+    }
 
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.ld_activityScreenTitles);
+    }
 
-        drawerFragment = (FragmentDrawer) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp(R.id.fragment_navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout), null);
-        drawerFragment.setDrawerListener(this);
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.ld_activityScreenIcons);
+        Drawable[] icons = new Drawable[ta.length()];
+        for (int i = 0; i < ta.length(); i++) {
+            int id = ta.getResourceId(i, 0);
+            if (id != 0) {
+                icons[i] = ContextCompat.getDrawable(this, id);
+            }
+        }
+        ta.recycle();
+        return icons;
+    }
 
-
-
-        List<String> dataList = new ArrayList<>();
-
-
-
+    @ColorInt
+    private int color(@ColorRes int res) {
+        return ContextCompat.getColor(this, res);
     }
 
     private Context getContext() {
@@ -67,25 +157,28 @@ public class MainActivity extends CustomActivity implements FragmentDrawer.Fragm
     }
 
     private void setupUiElements() {
-        tv_home = (TextView) findViewById(R.id.tv_home);
+        /*tv_home = (TextView) findViewById(R.id.tv_home);
         tv_tiffin = (TextView) findViewById(R.id.tv_tiffin);
         tv_cart = (TextView) findViewById(R.id.tv_cart);
-        tv_more = (TextView) findViewById(R.id.tv_more);
-        navBtn = (ImageButton) findViewById(R.id.nav_drawer_btn);
+        tv_more = (TextView) findViewById(R.id.tv_more);*/
+        // navBtn = (ImageButton) findViewById(R.id.nav_drawer_btn);
 
-
+        img_home = (ImageView) findViewById(R.id.img_home);
+        img_tiffin = (ImageView) findViewById(R.id.img_home);
+        img_cart = (ImageView) findViewById(R.id.img_home);
+        img_more = (ImageView) findViewById(R.id.img_home);
         setClick(R.id.rl_tab_1);
         setClick(R.id.rl_tab_2);
         setClick(R.id.rl_tab_3);
         setClick(R.id.rl_tab_4);
-        setClick(R.id.nav_drawer_btn);
+        // setClick(R.id.nav_drawer_btn);
 
-        tv_home.setSelected(true);
-        tv_home.setTextColor(Color.parseColor("#166DB6"));
+        /*tv_home.setSelected(true);
+        tv_home.setTextColor(Color.parseColor("#166DB6"));*/
 
     }
 
-    @Override
+  /*  @Override
     public void onDrawerItemSelected(View view, int position) {
 
         if (position == 0) {
@@ -117,18 +210,23 @@ public class MainActivity extends CustomActivity implements FragmentDrawer.Fragm
         } else if (position == 8) {
             MyApp.showMassage(getContext(), "will switch to service mode");
         }
-    }
-
+    }*/
 
 
     public void onClick(View v) {
         super.onClick(v);
         if (v.getId() == R.id.rl_tab_1) {
 
-            tv_home.setSelected(true);
+           /* tv_home.setSelected(true);
             tv_tiffin.setSelected(false);
             tv_cart.setSelected(false);
-            tv_more.setSelected(false);
+            tv_more.setSelected(false);*/
+
+            img_home.setSelected(true);
+            img_tiffin.setSelected(false);
+            img_cart.setSelected(false);
+            img_more.setSelected(false);
+
 
       /*      tv_home.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.search_active, 0, 0);
             tv_cart.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.notifications_inactive, 0, 0);
@@ -136,33 +234,42 @@ public class MainActivity extends CustomActivity implements FragmentDrawer.Fragm
             tv_more.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.account_inactive, 0, 0);*/
 
 
-            tv_home.setTextColor(Color.parseColor("#166DB6"));
+           /* tv_home.setTextColor(Color.parseColor("#166DB6"));
             tv_tiffin.setTextColor(Color.parseColor("#888F8C"));
             tv_cart.setTextColor(Color.parseColor("#888F8C"));
-            tv_more.setTextColor(Color.parseColor("#888F8C"));
+            tv_more.setTextColor(Color.parseColor("#888F8C"));*/
         } else if (v.getId() == R.id.rl_tab_2) {
-            tv_home.setSelected(false);
+            /*tv_home.setSelected(false);
             tv_tiffin.setSelected(true);
             tv_cart.setSelected(false);
-            tv_more.setSelected(false);
+            tv_more.setSelected(false);*/
+
+            img_home.setSelected(false);
+            img_tiffin.setSelected(true);
+            img_cart.setSelected(false);
+            img_more.setSelected(false);
 
           /*  tv_home.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.search_inactive, 0, 0);
             tv_cart.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.notifications_inactive, 0, 0);
             tv_tiffin.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.services_active, 0, 0);
             tv_more.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.account_inactive, 0, 0);*/
 
-            tv_home.setTextColor(Color.parseColor("#888F8C"));
+          /*  tv_home.setTextColor(Color.parseColor("#888F8C"));
             tv_tiffin.setTextColor(Color.parseColor("#166DB6"));
             tv_cart.setTextColor(Color.parseColor("#888F8C"));
             tv_more.setTextColor(Color.parseColor("#888F8C"));
-
+*/
             //startActivity(new Intent(MainActivity.this, AllServicesActivity.class));
 
         } else if (v.getId() == R.id.rl_tab_3) {
-            tv_home.setSelected(false);
+            /*tv_home.setSelected(false);
             tv_tiffin.setSelected(false);
             tv_cart.setSelected(true);
-            tv_more.setSelected(false);
+            tv_more.setSelected(false);*/
+            img_home.setSelected(false);
+            img_tiffin.setSelected(false);
+            img_cart.setSelected(true);
+            img_more.setSelected(false);
 
            /* tv_home.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.search_inactive, 0, 0);
             tv_cart.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.notifications_active, 0, 0);
@@ -170,17 +277,22 @@ public class MainActivity extends CustomActivity implements FragmentDrawer.Fragm
             tv_more.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.account_inactive, 0, 0);*/
 
 
-            tv_home.setTextColor(Color.parseColor("#888F8C"));
+         /*   tv_home.setTextColor(Color.parseColor("#888F8C"));
             tv_tiffin.setTextColor(Color.parseColor("#888F8C"));
             tv_cart.setTextColor(Color.parseColor("#166DB6"));
             tv_more.setTextColor(Color.parseColor("#888F8C"));
-//            openImage();
-          //  startActivity(new Intent(MainActivity.this, NotificationActivity.class));
+//            openImage();*/
+            //  startActivity(new Intent(MainActivity.this, NotificationActivity.class));
         } else if (v.getId() == R.id.rl_tab_4) {
-            tv_home.setSelected(false);
+            /*tv_home.setSelected(false);
             tv_tiffin.setSelected(false);
             tv_cart.setSelected(false);
             tv_more.setSelected(true);
+*/
+            img_home.setSelected(false);
+            img_tiffin.setSelected(false);
+            img_cart.setSelected(false);
+            img_more.setSelected(true);
 
            /* tv_home.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.search_inactive, 0, 0);
             tv_cart.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.notifications_inactive, 0, 0);
@@ -188,20 +300,16 @@ public class MainActivity extends CustomActivity implements FragmentDrawer.Fragm
             tv_more.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.account_active, 0, 0);*/
 
 
-            tv_home.setTextColor(Color.parseColor("#888F8C"));
+           /* tv_home.setTextColor(Color.parseColor("#888F8C"));
             tv_tiffin.setTextColor(Color.parseColor("#888F8C"));
             tv_cart.setTextColor(Color.parseColor("#888F8C"));
-            tv_more.setTextColor(Color.parseColor("#166DB6"));
+            tv_more.setTextColor(Color.parseColor("#166DB6"));*/
             //  startActivity(new Intent(MainActivity.this, AddMoneyActivity.class));
 
             // startActivity(new Intent(MainActivity.this, ScheduleServiceActivity.class));
 
-        }  else if (v == navBtn) {
-            drawer.openDrawer(GravityCompat.START);
-
         }
     }
-
 
 
     @Override
---
+++ app/src/main/java/com/localfriend/fragments/TiffinFragment.java
@@ -0,0 +1,74 @@
+package com.localfriend.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.localfriend.R;
+
+
+public class TiffinFragment extends Fragment implements View.OnClickListener {
+    CardView card_breakfast, card_lunch, card_dinner, card_snacks;
+
+    public TiffinFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView = inflater.inflate(R.layout.fragment_tiffin, container, false);
+        card_breakfast=(CardView)myView.findViewById(R.id.card_breakfast);
+        card_lunch=(CardView)myView.findViewById(R.id.card_lunch);
+        card_dinner=(CardView)myView.findViewById(R.id.card_dinner);
+        card_snacks=(CardView)myView.findViewById(R.id.card_snacks);
+        card_breakfast.setOnClickListener(this);
+        card_lunch.setOnClickListener(this);
+        card_dinner.setOnClickListener(this);
+        card_snacks.setOnClickListener(this);
+        return myView;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.card_breakfast:
+            {
+                Toast.makeText(getActivity(), "Order Breakfast", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.card_lunch:
+            {
+                Toast.makeText(getActivity(), "Order Lunch", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.card_dinner:
+            {
+                Toast.makeText(getActivity(), "Order Dinner", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.card_snacks:
+            {
+                Toast.makeText(getActivity(), "Order Snacks", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+    }
+}
